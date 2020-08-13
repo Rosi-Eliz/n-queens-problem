95b6be9c4b8be66b0e6248707652c54ec42765d6
Rosi-Eliz <rosi_eliz14@yahoo.bg>
2020-08-13 13:58:06 +0200

    Implements recursive solution to the N-Queen problem given a n x n chess board

---
+++ n-queens-problem/n-queens-problem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  n-queens-problem
+//
+//  Created by Rosi-Eliz Dzhurkova on 08.08.20.
+//  Copyright © 2020 Rosi-Eliz Dzhurkova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
